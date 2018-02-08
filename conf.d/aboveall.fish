b2e44bdf616ed91a6cc6beee9f881498efae876a
Pavel Popov <pavel@findhotel.net>
2018-02-08 10:57:56 +0100

    Disable fzf legacy keybindings

--- conf.d/aboveall.fish
+++ conf.d/aboveall.fish
@@ -2,6 +2,7 @@ alias gs="git status"
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
-set -gx PATH $PATH ~/go/bin
-set -g GOPATH ~/go
-set -g PYTHONDONTWRITEBYTECODE 1
+set -Ux PATH $PATH ~/go/bin
+set -U GOPATH ~/go
+set -U PYTHONDONTWRITEBYTECODE 1
+set -U FZF_LEGACY_KEYBINDINGS 0
