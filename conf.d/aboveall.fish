8046be416ebd00128b691e2344a82c19a76b5e6d
Pavel Popov <pavelpopov@outlook.com>
2018-02-13 10:52:19 +0100

    Add vi alias

--- conf.d/aboveall.fish
+++ conf.d/aboveall.fish
@@ -1,7 +1,9 @@
 alias gs="git status"
 alias ...="cd ../.."
 alias ....="cd ../../.."
+alias vi=nvim
 
+set -U EDITOR vi
 set -Ux PATH $PATH ~/go/bin
 set -U GOPATH ~/go
 set -U PYTHONDONTWRITEBYTECODE 1
