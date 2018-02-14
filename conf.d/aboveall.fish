feee549131de40aba6659b5fef0c5b49465c0a87
Pavel Popov <pavelpopov@outlook.com>
2018-02-14 17:26:20 +0100

    Update aboveall.fish

--- conf.d/aboveall.fish
+++ conf.d/aboveall.fish
@@ -3,8 +3,6 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias vi=nvim
 
-set -U EDITOR vi
-set -Ux PATH $PATH ~/go/bin
-set -U GOPATH ~/go
-set -U PYTHONDONTWRITEBYTECODE 1
-set -U FZF_LEGACY_KEYBINDINGS 0
+set -g GOPATH ~/go
+set -g PYTHONDONTWRITEBYTECODE 1
+set -g FZF_LEGACY_KEYBINDINGS 0
