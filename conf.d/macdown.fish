73f956cf07f0598ce648b371c68a0cdf36178a0c
Pavel Popov <pavelpopov@outlook.com>
2018-05-07 18:48:13 +0200

    Update macdown.fish

--- conf.d/macdown.fish
+++ conf.d/macdown.fish
@@ -1,4 +1,3 @@
 function macdown --description 'Open markdown document in MacDown'
-    set -lx DIR (mdfind kMDItemCFBundleIdentifier=com.uranusjr.macdown | head -n1)
-    $DIR/Contents/SharedSupport/bin/macdown $argv
+    open -a MacDown $argv;
 end
