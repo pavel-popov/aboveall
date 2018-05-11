85b5e1ca1acb9f8bcc4e6bbf06d98e4f8f26ca2a
Pavel Popov <pavel@findhotel.net>
2018-05-11 18:52:19 +0200

    Fix posix-source.fish

--- conf.d/posix-source.fish
+++ conf.d/posix-source.fish
@@ -1,6 +1,8 @@
 function posix-source
   for i in (cat $argv | grep = | grep -v -e '^#')
     set arr (string split -m1 = $i)
-    set -gx $arr[1] $arr[2]
+    set key $arr[1]
+    set val (echo $arr[2] | sed 's/^[\'"]//;s/[\'"]$//')
+    set -gx $key $val
   end
 end
