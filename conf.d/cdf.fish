0410aa5bdc8382b465fef5ca2b14a2a60860ef77
Pavel Popov <pavel@findhotel.net>
2018-09-27 21:57:12 +0200

    Use numeric sorting

--- conf.d/cdf.fish
+++ conf.d/cdf.fish
@@ -1,3 +1,3 @@
 function cdf --description 'Change directory from z recents using fzy to fuzzy selection'
-     cd (z -l $argv | sort -r | fzy | cut -c 12-)
+     cd (z -l $argv | sort -r -n | fzy | cut -c 12-)
 end
