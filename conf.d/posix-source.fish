9ef01897896fdb3b58d4bc224d80f879edba5561
Pavel Popov <pavel@findhotel.net>
2018-12-21 12:59:57 +0100

    Revert back global scope

--- conf.d/posix-source.fish
+++ conf.d/posix-source.fish
@@ -3,6 +3,7 @@ function posix-source
     set arr (string split -m1 = $i)
     set key $arr[1]
     set val (echo $arr[2] | sed 's/^[\'"]//;s/[\'"]$//')
-    set -x $key $val
+    echo "setting variable $key"
+    set -gx $key $val
   end
 end
